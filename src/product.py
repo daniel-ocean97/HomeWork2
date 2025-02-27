6f0272cf7f51c815409cc147fa3a7c9ba637cd92
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-02-27 16:00:15 +0300

    HomeWork 15.1

--- src/product.py
+++ src/product.py
@@ -12,6 +12,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.\n"
+
+    def __add__(self, other):
+        return round(self.__price * self.quantity + other.__price * other.quantity, 2)
+
     @classmethod
     def new_product(cls, product_data):
         return cls(
@@ -50,6 +56,16 @@ class Category:
         Category.number_of_categories += 1
         self.product_count = len(products)
 
+    def __str__(self):
+        total_quantity = 0
+        for product in self.__products:
+            total_quantity += product.quantity
+        return f"{self.name.title()}, количество продуктов: {total_quantity}"
+
+    @property
+    def products_in_list(self):
+        return self.__products
+
     def add_product(self, product):
         if isinstance(product, Product):
             self.__products.append(product)
@@ -63,9 +79,25 @@ class Category:
     def products(self):
         result = ""
         for product in self.__products:
-            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            result += str(product)
         return result
 
 
-if __name__ == "__main__":
-    pass
+class ProductIterator:
+    """Класс итератор для перебора продуктов в классе Category"""
+
+    def __init__(self, category_obj):
+        self.category = category_obj
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.category.products_in_list):
+            product = self.category.products_in_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
